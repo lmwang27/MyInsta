efa432f703a77f995b1037b028fddb478c04e4f6
lmwang27 <lmwang27@gmail.com>
2019-12-09 14:48:43 -0800

    adding feature of reading images

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include , path
-from mini_insta.views import HelloDjango
+from mini_insta.views import HelloDjango , PostsView
 
 urlpatterns = [
-    path('', HelloDjango.as_view(), name='test')
+    path('', HelloDjango.as_view(), name='test'),
+    path('posts/',PostsView.as_view(), name = 'posts')
 ]
--- mini_insta/views.py
+++ mini_insta/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView , ListView, DetailView
+from mini_insta.models import Post
 
 # Create your views here.
 # render html
 class HelloDjango(TemplateView):
     template_name = 'test.html'
-    
+
+class PostsView(ListView):
+    model = Post
+    template_name = "index.html"
+
