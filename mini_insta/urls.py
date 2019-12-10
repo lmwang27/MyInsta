de4e52e04c14b113d8ee467084255fbfddab3677
lmwang27 <lmwang27@gmail.com>
2019-12-09 16:31:33 -0800

    adding django-form creatView

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -1,8 +1,9 @@
 from django.urls import include , path
-from mini_insta.views import HelloDjango , PostsView , PostDetailView
+from mini_insta.views import HelloDjango , PostsView , PostDetailView, PostCreateView
 
 urlpatterns = [
     path('', HelloDjango.as_view(), name='test'),
     path('posts/',PostsView.as_view(), name = 'posts'),
     path('post/<int:pk>',PostDetailView.as_view(), name = 'post_detail'),
+    path('post/new/',PostCreateView.as_view(), name = 'post_upload'),
 ]
--- mini_insta/views.py
+++ mini_insta/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView , ListView, DetailView
+from django.views.generic import TemplateView, ListView, DetailView
 from mini_insta.models import Post
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
 
 # Create your views here.
 # render html
@@ -15,5 +17,11 @@ class PostsView(ListView):
 class PostDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = "post_create.html"
+    fields = '__all__'
+
 
 
