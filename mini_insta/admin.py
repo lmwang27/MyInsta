333c308b9045e5888c046a663e8c36e97493de7f
lmwang27 <lmwang27@gmail.com>
2019-12-11 15:32:26 -0800

    adding likes

--- mini_insta/admin.py
+++ mini_insta/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from mini_insta.models import Post, InstaUser
+from mini_insta.models import Post, InstaUser, Like, Comment
 
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(InstaUser)
+admin.site.register(Like)
+admin.site.register(Comment)
